bf6645df4385122cecad6262c4ac3db4354c57ce
KalanaHE <kalanah1996@gmail.com>
2022-01-02 12:54:12 +0530

    firebase config done

--- app/EntryPoint.js
+++ app/EntryPoint.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {Alert} from 'react-native';
 import {Provider} from 'react-redux';
 import store from './store';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -6,8 +7,23 @@ import {Provider as PaperProvider} from 'react-native-paper';
 import FlashMessage from './CommonComponents/FlashMessage';
 import Navigator from './navigation';
 import {theme} from './core/theme';
+import {requestUserMessagingPermission} from './utils/devices/messaging';
+import messaging from '@react-native-firebase/messaging';
 
 const Entrypoint = () => {
+  useEffect(() => {
+    requestUserMessagingPermission();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', remoteMessage);
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Provider store={store}>
       <SafeAreaProvider>
--- index.js
+++ index.js
@@ -6,5 +6,10 @@ import {AppRegistry} from 'react-native';
 import App from './app/EntryPoint.js';
 import {name as appName} from './app.json';
 import 'react-native-gesture-handler';
+import messaging from '@react-native-firebase/messaging';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+  console.log('Message handled in the background!', remoteMessage);
+});
 
 AppRegistry.registerComponent(appName, () => App);
